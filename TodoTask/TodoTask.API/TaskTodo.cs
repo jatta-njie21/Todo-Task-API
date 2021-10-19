a4469ec9d9f42ceb120e02152c7f2f5920fcb8b6
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-19 15:20:02 +0000

    Added      TodoTask/TodoTask.API/Controllers/TodoTaskController.cs Added      TodoTask/TodoTask.API/TaskTodo.cs Added      TodoTask/TodoTask.API/TodoTask.API.csproj.user Added      TodoTask/TodoTask.API/obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.cs Added      TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.AssemblyInfo.cs Added      TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.AssemblyInfoInputs.cache Added      TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.GeneratedMSBuildEditorConfig.editorconfig Added      TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.assets.cache Added      TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.csproj.AssemblyReference.cache Added      TodoTask/TodoTask.API/obj/TodoTask.API.csproj.nuget.dgspec.json Added      TodoTask/TodoTask.API/obj/TodoTask.API.csproj.nuget.g.props Added      TodoTask/TodoTask.API/obj/TodoTask.API.csproj.nuget.g.targets Added      TodoTask/TodoTask.API/obj/project.assets.json Added      TodoTask/TodoTask.API/obj/project.nuget.cache

---
+++ TodoTask/TodoTask.API/TaskTodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoTask.API
+{
+    public class TaskTodo
+    {
+        public int ID { get; set; }
+        public string TaskName { get; set; }
+        public string Descripion { get; set; }
+    }
+}
