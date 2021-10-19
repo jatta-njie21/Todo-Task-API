09e1a541c727d1bf194eda1417ae6d5bbdb4c47c
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-10-19 17:29:11 +0000

    Added      TodoTask/TodoTask.API/TodoTask.cs Modified   TodoTask/TodoTask.API/Controllers/TodoTaskController.cs Modified   TodoTask/TodoTask.API/obj/Debug/net5.0/TodoTask.API.csproj.AssemblyReference.cache Missing    TodoTask/TodoTask.API/TaskTodo.cs

--- TodoTask/TodoTask.API/Controllers/TodoTaskController.cs
+++ TodoTask/TodoTask.API/Controllers/TodoTaskController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace TodoTask.API.Controllers
 {
@@ -11,6 +8,51 @@ namespace TodoTask.API.Controllers
     [ApiController]
     public class TodoTaskController : ControllerBase
     {
+        List<TodoTask> Tasks = new();
+        public TodoTaskController()
+        {
+            Tasks = new List<TodoTask>()
+            {
+                new TodoTask{ID = 1, TaskName = "Write and publish an article", Descripion = "Just a simple API article."},
+                new TodoTask{ID = 2, TaskName = "Push code to github", Descripion = "Make the code available on github."}
+            };
+        }
 
+        // GET: api/values
+        [HttpGet]
+        [Route("GetAllTodoTasks")]
+        public IActionResult GetAllTodoTaskList()
+        {
+            //return a list of all the tasks
+            return Ok(Tasks);
+        }
+
+        // POST: api/values
+        [HttpPost]
+        [Route("CreateNewTodoTask")]
+        public IActionResult CreateNewTodoTask(TodoTask todoTask)
+        {
+            Tasks.Add(todoTask);
+
+            //creates and returns a new task
+            return Ok(Tasks);
+        }
+
+        // GET: api/values/2
+        [HttpGet]
+        [Route("GetTodoTaskByID/{ID}")]
+        public IActionResult GetTodoTaskByID(int id)
+        {
+            var task = (from t in Tasks where t.ID == id select t).ToList();
+
+            //returns nothing if the id is not found
+            if (task.Count == 0) 
+            {
+                NoContent();
+            }               
+
+            //returns a specific task based on the id
+            return Ok(Tasks);
+        }
     }
 }
